0062e3c19716a0b5793b7f9825b2f069311560d6
Francisco Ramírez <py.francisco.ramirez@gmail.com>
2020-09-15 18:38:03 -0500

    Trying to merge all the ip functions ins a single module.

--- func_registered_ip.py
+++
@@ -1,6 +0,0 @@
-#! /Users/Francisco/Documents/ATBSWP/ATBSWP_ve/bin/python
-
-def read_registered_ip():
-    with open('/Users/Francisco/Documents/ATBSWP/ip_updater/public_ip.txt') as ip:
-        registered_ip = ip.read()
-        return registered_ip
---
+++ ip_tools.py
@@ -0,0 +1,18 @@
+#! /Users/Francisco/Documents/ATBSWP/ATBSWP_ve/bin/python
+import requests
+
+# Refactor to use relative paths instead of absolute ones.
+
+def get_public_ip():
+    res = requests.get('http://ip4only.me/api/')
+    public_ip = res.text.split(',')[1]
+    return public_ip
+
+def read_registered_ip():
+    with open('/Users/Francisco/Documents/ATBSWP/ip_updater/public_ip.txt') as ip:
+        registered_ip = ip.read()
+        return registered_ip
+
+def update_registered_ip(new_ip_address):
+    with open('/Users/Francisco/Documents/ATBSWP/ip_updater/public_ip.txt', 'w') as ip:
+        ip.write(new_ip_address)
